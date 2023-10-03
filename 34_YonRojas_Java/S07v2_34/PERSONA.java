91c4928b6571789088f024d38cb27f95b7d6829c
YonARN_34 <yon.rojas@vallegrande.edu.pe>
2023-10-03 10:06:32 -0500

    Add files via upload

---
+++ 34_YonRojas_Java/S07v2_34/PERSONA.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package S07v2_34;
+
+/**
+ *
+ * @author Lab31
+ */
+public class PERSONA {
+    
+}
---
+++ 34_YonRojas_Java/S07v2_34/PERSONA1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package S07v2_34;
+
+/**
+ *
+ * @author Lab31
+ */
+public class PERSONA1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
