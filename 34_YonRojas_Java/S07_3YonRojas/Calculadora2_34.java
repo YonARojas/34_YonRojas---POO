d0011f3f1ce3b58c4cf5622b354be86857df7039
YonARN_34 <yon.rojas@vallegrande.edu.pe>
2023-10-03 09:16:58 -0500

    S07

---
+++ 34_YonRojas_Java/S07_3YonRojas/Calculadora2_34.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package S07_3YonRojas;
+
+/**
+ *
+ * @author Lab31
+ */
+public class Calculadora2_34 {
+    //atributos
+    int c;
+    int d;
+    String suma;
+    String resta;
+    //metodos
+public String suma () {
+    return "5";
+}
+public String resta() {
+    return "1";
+}
+}
